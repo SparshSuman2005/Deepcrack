e6b6347a35ee56baa6723af983cac7ac89d20edb
sparsh suman <sparsh.storage1@gmail.com>
2026-09-16 12:36:11 +0530

    Add .gitignore and initial README.md; implement image loading and visualization script

---
+++ preprocessing/know_about.py
@@ -0,0 +1,25 @@
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+image = cv2.imread(r"D:\DeepCrack\dataset\train_img\7Q3A9060-1.jpg")
+mask = cv2.imread(r"D:\DeepCrack\dataset\train_lab\7Q3A9060-1.png", cv2.IMREAD_GRAYSCALE)
+
+print("Image loaded:", image is not None)
+print("Mask loaded:", mask is not None)
+
+image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
+
+plt.figure(figsize=(10, 4))
+
+plt.subplot(1, 2, 1)
+plt.imshow(image)
+plt.title("Original Image")
+plt.axis("off")
+
+plt.subplot(1, 2, 2)
+plt.imshow(mask, cmap="gray")
+plt.title("Ground Truth Mask")
+plt.axis("off")
+
+plt.show()
